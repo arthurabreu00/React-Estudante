2277cc923c601b482c61b381a344d490295a9cb9
Arthur Tavares <arthurabreu00@gmail.com>
2019-03-17 15:32:17 -0300

    Desistindo do projeto

--- marketplace/src/components/Navbar/style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-
-const Navbar = styled.nav`
-    
-    padding-top: 10px;
-`;
-
-export default Navbar;
--- marketplace/src/objects/IconsSocial/Style.js
+++
@@ -1,9 +0,0 @@
-import Styled from 'styled-components';
-
-export const Icons = Styled.span`
-    margin-right:32px;
-    font-size: 1.2rem;
-    color: #828282;
-`
-
-export default Icons;
